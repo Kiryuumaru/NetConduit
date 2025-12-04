dbe4a35aac4df5ece017c28174842c4821ede674
Clynt Neiko Rupinta <neiko@meldcx.com>
2025-12-04 17:36:56 +0800

    Refactor build pipeline and enhance CI/CD workflow
    Refactored `Build.cs` to support matrix-based builds and tests for multiple deployment apps (`net_conduit`, `net_conduit_tcp`, `net_conduit_websocket`). Added `DeploymentAppSpec` for app specifications and secret variables for secure authentication. Introduced `NormalizeReleaseNotes` helper for release note encoding.
    
    Updated `.csproj` files to enable dynamic versioning and improve package metadata. Integrated `GitHubActionsTestLogger` for enhanced test reporting.
    
    Added a new GitHub Actions workflow (`nuke-cicd.yml`) with pre-setup, build, test, and publish stages. Configured caching, artifact management, and concurrency control. Improved modularity, scalability, and test coverage across the pipeline.

--- build/Build.cs
+++ build/Build.cs
@@ -1,11 +1,15 @@
 using Nuke.Common;
 using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
 using NukeBuildHelpers;
 using NukeBuildHelpers.Common.Attributes;
+using NukeBuildHelpers.Common.Enums;
 using NukeBuildHelpers.Entry;
 using NukeBuildHelpers.Entry.Extensions;
 using NukeBuildHelpers.Runner.Abstraction;
 using Serilog;
+using System;
 using System.Linq;
 
 class Build : BaseNukeBuildHelpers
@@ -16,32 +20,116 @@ class Build : BaseNukeBuildHelpers
 
     public override string MainEnvironmentBranch => "master";
 
+    DeploymentAppSpec[] DeploymentApps =>
+    [
+        new DeploymentAppSpec
+        {
+            AppId = "net_conduit",
+            ProjectName = "NetConduit",
+            ProjectTestName = "NetConduit.UnitTests"
+        },
+        new DeploymentAppSpec
+        {
+            AppId = "net_conduit_tcp",
+            ProjectName = "NetConduit.Tcp",
+            ProjectTestName = "NetConduit.Tcp.IntegrationTests"
+        },
+        new DeploymentAppSpec
+        {
+            AppId = "net_conduit_websocket",
+            ProjectName = "NetConduit.WebSocket",
+            ProjectTestName = "NetConduit.WebSocket.IntegrationTests"
+        }
+    ];
+
+    [SecretVariable("NUGET_AUTH_TOKEN")]
+    readonly string? NuGetAuthToken;
+
     [SecretVariable("GITHUB_TOKEN")]
     readonly string? GithubToken;
 
-    BuildEntry BuildEntry => _ => _
-        .AppId("sample_app")
-        .RunnerOS(RunnerOS.Windows2022)
-        .Execute(() =>
-        {
-            // build logic here
-        });
-
     TestEntry TestEntry => _ => _
-        .AppId("sample_app")
         .RunnerOS(RunnerOS.Ubuntu2204)
-        .Execute(() =>
-        {
-            // test logic here
-        });
+        .ExecuteBeforeBuild(true)
+        .Matrix(DeploymentApps, (test, spec) => test
+            .AppId(spec.AppId)
+            .WorkflowId($"{spec.AppId}_test")
+            .DisplayName($"Test {spec.ProjectName}")
+            .Execute(() =>
+            {
+                DotNetTasks.DotNetBuild(_ => _
+                    .SetProjectFile(RootDirectory / "tests" / spec.ProjectTestName / $"{spec.ProjectTestName}.csproj")
+                    .SetConfiguration("Release"));
+                DotNetTasks.DotNetTest(_ => _
+                    .SetProcessAdditionalArguments(
+                        "--logger \"GitHubActions;summary.includePassedTests=true;summary.includeSkippedTests=true\" " +
+                        "-- " +
+                        "RunConfiguration.CollectSourceInformation=true " +
+                        "DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Format=opencovere ")
+                    .SetProjectFile(RootDirectory / "tests" / spec.ProjectTestName / $"{spec.ProjectTestName}.csproj")
+                    .SetConfiguration("Release"));
+            }));
+
+    BuildEntry BuildEntry => _ => _
+        .RunnerOS(RunnerOS.Ubuntu2204)
+        .Matrix(DeploymentApps, (test, spec) => test
+            .AppId(spec.AppId)
+            .WorkflowId($"{spec.AppId}_build")
+            .DisplayName($"Build {spec.ProjectName}")
+            .Execute(context =>
+            {
+                var app = context.Apps.Values.First();
+                string version = app.AppVersion.Version.ToString()!;
+                string? releaseNotes = null;
+                if (app.BumpVersion != null)
+                {
+                    version = app.BumpVersion.Version.ToString();
+                    releaseNotes = app.BumpVersion.ReleaseNotes;
+                }
+                else if (app.PullRequestVersion != null)
+                {
+                    version = app.PullRequestVersion.Version.ToString();
+                }
+                app.OutputDirectory.DeleteDirectory();
+                DotNetTasks.DotNetClean(_ => _
+                    .SetProject(RootDirectory / "ApplicationBuilderHelpers" / "ApplicationBuilderHelpers.csproj"));
+                DotNetTasks.DotNetBuild(_ => _
+                    .SetProjectFile(RootDirectory / "ApplicationBuilderHelpers" / "ApplicationBuilderHelpers.csproj")
+                    .SetConfiguration("Release"));
+                DotNetTasks.DotNetPack(_ => _
+                    .SetProject(RootDirectory / "ApplicationBuilderHelpers" / "ApplicationBuilderHelpers.csproj")
+                    .SetConfiguration("Release")
+                    .SetNoRestore(true)
+                    .SetNoBuild(true)
+                    .SetIncludeSymbols(true)
+                    .SetSymbolPackageFormat("snupkg")
+                    .SetVersion(version)
+                    .SetPackageReleaseNotes(NormalizeReleaseNotes(releaseNotes))
+                    .SetOutputDirectory(app.OutputDirectory));
+            }));
 
     PublishEntry PublishEntry => _ => _
-        .AppId("sample_app")
         .RunnerOS(RunnerOS.Ubuntu2204)
-        .Execute(context =>
-        {
-            // publish logic here
-        });
+        .Matrix(DeploymentApps, (test, spec) => test
+            .AppId(spec.AppId)
+            .WorkflowId($"{spec.AppId}_publish")
+            .DisplayName($"Publish {spec.ProjectName}")
+            .Execute(async context =>
+            {
+                var app = context.Apps.Values.First();
+                if (app.RunType == RunType.Bump)
+                {
+                    DotNetTasks.DotNetNuGetPush(_ => _
+                        .SetSource("https://nuget.pkg.github.com/kiryuumaru/index.json")
+                        .SetApiKey(GithubToken)
+                        .SetTargetPath(app.OutputDirectory / "**"));
+                    DotNetTasks.DotNetNuGetPush(_ => _
+                        .SetSource("https://api.nuget.org/v3/index.json")
+                        .SetApiKey(NuGetAuthToken)
+                        .SetTargetPath(app.OutputDirectory / "**"));
+                    await AddReleaseAsset(app.OutputDirectory, app.AppId);
+                }
+            }));
 
     Target Clean => _ => _
         .Executes(() =>
@@ -58,4 +146,19 @@ class Build : BaseNukeBuildHelpers
             }
             (RootDirectory / ".vs").DeleteDirectory();
         });
+
+    private string? NormalizeReleaseNotes(string? releaseNotes)
+    {
+        return releaseNotes?
+            .Replace(",", "%2C")?
+            .Replace(":", "%3A")?
+            .Replace(";", "%3B");
+    }
+
+    class DeploymentAppSpec
+    {
+        public required string AppId { get; set; }
+        public required string ProjectName { get; set; }
+        public required string ProjectTestName { get; set; }
+    }
 }
