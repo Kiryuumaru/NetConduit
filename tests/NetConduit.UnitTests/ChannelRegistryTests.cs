012cffe416f42b5d42fec815e5c163517ffe0729
Clynt Neiko Rupinta <neiko@meldcx.com>
2026-05-21 18:17:54 +0800

    fix(transport/ipc): dispose client/socket on post-connect and post-accept setup failure (fixes #304)

--- tests/NetConduit.UnitTests/ChannelRegistryTests.cs
+++ tests/NetConduit.UnitTests/ChannelRegistryTests.cs
@@ -58,5 +58,6 @@ public sealed class ChannelRegistryTests
         public void NotifyPendingAcceptCancelled(string channelId) { }
         public void NotifyChannelOpened(string channelId) { }
         public void SendAck(ushort channelIndex, ulong consumedPosition) { }
+        public void NotifyEventHandlerException(Exception ex) { }
     }
 }
